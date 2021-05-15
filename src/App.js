1737001429e38988d3993746b1ec01162e7d5edf
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-05-16 00:20:29 +1000

    add styling to button and text field

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import CanvasDraw from "react-canvas-draw"
 import {useRef} from "react"
@@ -16,9 +16,9 @@ const App = () => {
   
   return (
     <div className="flex flex-col items-center">
-    <input type="text"/>
-     <CanvasDraw ref={canvasRef} hideGrid={true} canvasWidth={window.screen.availWidth/2} canvasHeight={window.screen.availHeight/2} className="border border-black rounded-md"/>
-     <button onClick = {handleClick}>Save</button>
+    <input type="text" className="mb-4 mt-4 border border-black rounded-md"/>
+     <CanvasDraw ref={canvasRef} hideGrid={true} canvasWidth={window.screen.availWidth/2} canvasHeight={window.screen.availHeight/2} className="border border-black rounded-md mb-4"/>
+     <button onClick = {handleClick} className="border border-black rounded-md p-4">Save drawing</button>
      </div>
       
   );
