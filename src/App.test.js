2cd494ba7f3cb4978112f38d250cd55484c11249
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-05-16 12:17:11 +1000

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {shallow} from "enzyme"
 
 it("renders without crashing", () => {
-  shallow(<App/>)
+  render(<App/>)
 })
