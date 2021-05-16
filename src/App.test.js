f25e05971021d212bef9fdfbb2549eb333773912
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-05-16 21:43:56 +1000

    Update App.test.js (#8)
    * Update App.test.js
    
    * add missing import
    
    * fix test
    
    * fix test text recognition
    
    * Update App.test.js
    
    * Update App.test.js
    
    * update test
    
    * fix missing text
    
    * Update App.test.js
    
    * fix missing word
    
    * create button component fix button click test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import App from './App';
+import Button from "./components/Button"
 
 it("renders without crashing", () => {
   render(<App/>)
 })
 
 test('types inside input', () => {
-  document.body.innerHTML = `<input/>`
+  render(<App/>)
 
   userEvent.type(screen.getByRole('textbox'), 'Hello, World!')
   expect(screen.getByRole('textbox')).toHaveValue('Hello, World!')
+})
+
+test('calls onClick when clicked', () => {
+  const handleClickMock = jest.fn()
+  render(<Button handleClick={handleClickMock}/>)
+  
+  
+  fireEvent.click(screen.getByText(/Save drawing/i))
+  expect(handleClickMock).toHaveBeenCalledTimes(1)
 })
