953cfb5468014c226aa6f64832ce438c15f2d1d7
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-05-16 23:27:01 +1000

    add title state, add change listener to input field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import CanvasDraw from "react-canvas-draw"
-import {useRef} from "react"
+import {useRef, useState} from "react"
 import Button from "./components/Button"
 
 const App = () => {
   const canvasRef = useRef(undefined)
+  const [title, setTitle] = useState("")
 
   
 
@@ -14,10 +15,13 @@ const App = () => {
     console.log(canvasRef.current.getSaveData());
    
 }
+const handleChange = (e) => {
+  setTitle(e.target.value)
+}
   
   return (
     <div className="flex flex-col items-center">
-    <input type="text" className="mb-4 mt-4 border border-black rounded-md"/>
+    <input type="text" onChange={handleChange} className="mb-4 mt-4 border border-black rounded-md"/>
      <CanvasDraw ref={canvasRef} hideGrid={true} canvasWidth={window.screen.availWidth/2} canvasHeight={window.screen.availHeight/2} className="border border-black rounded-md mb-4"/>
      <Button handleClick={handleClick}/>
      </div>
