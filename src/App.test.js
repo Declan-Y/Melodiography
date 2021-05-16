f7000073813fae6bcce927f74137e6b20ed6044a
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-05-16 14:08:11 +1000

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,3 +4,16 @@ import App from './App';
 it("renders without crashing", () => {
   render(<App/>)
 })
+
+test('types inside input', () => {
+  document.body.innerHTML = `<input/>`
+
+  userEvent.type(screen.getByRole('textbox'), 'Hello, World!')
+  expect(screen.getByRole('textbox')).toHaveValue('Hello, World!')
+})
+
+test('calls onClick prop when clicked', () => {
+  const handleClick = jest.fn()
+  fireEvent.click(screen.getByText("Save"))
+  expect(handleClick).toHaveBeenCalledTimes(1)
+})
