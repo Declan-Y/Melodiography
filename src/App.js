e448f8c8159d7396d5d1295c56c496085ec42012
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-05-16 13:50:27 +1000

    remove redundant aria role

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   
   return (
     <div className="flex flex-col items-center">
-    <input type="text" role="textbox" className="mb-4 mt-4 border border-black rounded-md"/>
+    <input type="text" className="mb-4 mt-4 border border-black rounded-md"/>
      <CanvasDraw ref={canvasRef} hideGrid={true} canvasWidth={window.screen.availWidth/2} canvasHeight={window.screen.availHeight/2} className="border border-black rounded-md mb-4"/>
      <button onClick = {handleClick} className="border border-black rounded-md p-4">Save drawing</button>
      </div>
