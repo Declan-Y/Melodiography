33791c162a3fa3cb6a4d67231470a1bd5781a6a4
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-05-16 23:51:59 +1000

    add clear and undo buttons

--- src/App.js
+++ src/App.js
@@ -14,12 +14,27 @@ const App = () => {
     console.log(canvasRef.current.getSaveData());
    
 }
+const handleUndo = () => {
+
+  canvasRef.current.undo()
+ 
+}
+const handleClear = () => {
+
+  canvasRef.current.clear()
+ 
+}
   
   return (
     <div className="flex flex-col items-center">
     <input type="text" className="mb-4 mt-4 border border-black rounded-md"/>
      <CanvasDraw ref={canvasRef} hideGrid={true} canvasWidth={window.screen.availWidth/2} canvasHeight={window.screen.availHeight/2} className="border border-black rounded-md mb-4"/>
-     <Button handleClick={handleClick}/>
+     <div className="flex justify-evenly w-1/4">
+     
+     <Button handleClick={handleUndo} name={'Undo'}/>
+     <Button handleClick={handleClear} name={'Clear'}/>
+     <Button handleClick={handleClick} name={'Save drawing'}/>
+     </div>
      </div>
       
   );
